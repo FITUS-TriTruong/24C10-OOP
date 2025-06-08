4640cb95fc8f87cacf5f92e339feb2ab215597fc
Tri Truong <tmtri2430@clc.fitus.edu.vn>
2025-06-09 02:35:05 +0700

    Genie updates on level 1, 2, 3

---
+++ Scripts/stage_1.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var entered=false;
+
+func _on_next_level_body_entered(_body: CharacterBody2D) -> void:
+	entered=true
+func _on_next_level_body_exited(_body) -> void:
+	entered=false
+
+func _process(delta) -> void:
+	if entered==true:
+			get_tree().change_scene_to_file("res://Class/Stage_2.tscn")
