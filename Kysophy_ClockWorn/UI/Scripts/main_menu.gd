f3756474348745dc4d630b2c822c4eebec07240e
Tri Truong <tmtri2430@clc.fitus.edu.vn>
2025-08-08 03:25:18 +0700

    Git ignore and cleanups, UI files
    - Add .gitignore and clean up Godot generated files
    - Added new UI components to project
    
    Files excluded:
    - Godot build cache (.godot/)
    - Asset import files (*.import)
    - Temporary files (*.tmp)
    - OS-specific files
    - IDE-specific files
    - Generated shader and build artifacts

---
+++ Kysophy_ClockWorn/UI/Scripts/main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_button_pressed() -> void:
+	print("Start button pressed.")
+	get_tree().change_scene_to_file("res://Class/Stage_1.tscn")
+
+func _on_option_button_pressed() -> void:
+	print("Option butotn pressed.")
+
+
+func _on_quit_button_pressed() -> void:
+	print("Quit button pressed.")
+	get_tree().quit()
