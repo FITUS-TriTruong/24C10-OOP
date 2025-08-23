2e3be8eac8d329512bb29c5e5a40fc1f76802b2a
Kysophy <khoas8232@gmail.com>
2025-08-23 12:39:40 +0700

    Fixed Minor issue being unable to interact with memories in stage 3

--- Kysophy-ClockWorn/Scripts/memories/memory_2.gd
+++ Kysophy-ClockWorn/Scripts/memories/memory_2.gd
@@ -1,13 +1,17 @@
 extends StaticBody2D
 
 @onready var interaction_area = $InteractionArea
-@onready var mem = $AnimatedSprite2D
+@onready var mem: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready():
+	print("Ready called for memory")
 	mem.play("default")
+	print("Mem animation started")
 	interaction_area.interact = Callable(self, "_key_acquire")
+	print("Interact assigned")
 	
 func _key_acquire():
+	print("Key acquired triggered")
 	DialogueManager.show_example_dialogue_balloon(load("res://Scripts/Dialogues/Mem2.dialogue"))
 	mem.visible = false
 	interaction_area.queue_free()
